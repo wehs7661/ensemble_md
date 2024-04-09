a7338c0ef1e2b120e83983631e4af7040adb6816
Wei-Tse Hsu <wehs7661@colorado.edu>
2024-04-10 01:42:22 +0800

    Added test_synthesize_data.py

---
+++ ensemble_md/tests/test_synthesize_data.py
@@ -0,0 +1,13 @@
+####################################################################
+#                                                                  #
+#    ensemble_md,                                                  #
+#    a python package for running GROMACS simulation ensembles     #
+#                                                                  #
+#    Written by Wei-Tse Hsu <wehs7661@colorado.edu>                #
+#    Copyright (c) 2022 University of Colorado Boulder             #
+#                                                                  #
+####################################################################
+"""
+Unit tests for the module synthesize_data.py.
+"""
+from ensemble_md.analysis import analyze_traj
