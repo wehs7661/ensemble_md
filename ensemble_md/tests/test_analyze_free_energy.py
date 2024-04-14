48b704218db7ab8f9c420fb770c8dbff3cb04265
Wei-Tse Hsu <wehs7661@colorado.edu>
2024-04-15 03:32:32 +0800

    Removed traj.xtc and initialized test_analyze_free_energy.py

---
+++ ensemble_md/tests/test_analyze_free_energy.py
@@ -0,0 +1,44 @@
+####################################################################
+#                                                                  #
+#    ensemble_md,                                                  #
+#    a python package for running GROMACS simulation ensembles     #
+#                                                                  #
+#    Written by Wei-Tse Hsu <wehs7661@colorado.edu>                #
+#    Copyright (c) 2022 University of Colorado Boulder             #
+#                                                                  #
+####################################################################
+"""
+Unit tests for the module analyze_free_energy.py.
+"""
+
+
+def test_preprocess_data():
+    pass
+
+
+def test_apply_estimators():
+    pass
+
+
+def test_calculate_df_adjacent():
+    pass
+
+
+def test_combine_df_adjacent():
+    pass
+
+
+def test_calculate_free_energy():
+    pass
+
+
+def test_calculate_df_rmse():
+    pass
+
+
+def test_plot_free_energy():
+    pass
+
+
+def test_average_weights():
+    pass
